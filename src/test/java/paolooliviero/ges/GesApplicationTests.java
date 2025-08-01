4e6f948ed958468be56f400e8ee5233e16cb8033
PaoloOliviero <paolo.oliviero3@gmail.com>
2025-08-01 09:44:56 +0200

    Initial Commit

---
+++ src/test/java/paolooliviero/ges/GesApplicationTests.java
@@ -0,0 +1,13 @@
+package paolooliviero.ges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
